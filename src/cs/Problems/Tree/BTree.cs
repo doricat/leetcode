264dc523f17c06c83adf79646a4d299462f3a9fb
inwlbty <inwlbty@users.noreply.github.com>
2019-03-26 19:21:41 +0800

    binary-tree-inorder-traversal

---
+++ src/cs/Problems/Tree/BTree.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Problems.Tree
+{
+    public class BTree
+    {
+        // https://leetcode.com/problems/binary-tree-inorder-traversal
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            InorderTraversal(root, result);
+            return result;
+        }
+
+        private static void InorderTraversal(TreeNode node, IList<int> list)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.left, list);
+                list.Add(node.val);
+                InorderTraversal(node.right, list);
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
---
+++ test/cs/Problems.Tests/BTreeTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Problems.Tree;
+
+namespace Problems.Tests
+{
+    public class BTreeTests
+    {
+        [Test]
+        public void InorderTraversalTest1()
+        {
+            var result = BTree.InorderTraversal(new TreeNode(1)
+            {
+                right = new TreeNode(2)
+                {
+                    left = new TreeNode(3)
+                }
+            });
+            Assert.AreEqual(result.ToArray(), new[] {1, 3, 2});
+        }
+    }
+}
