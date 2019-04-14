9f919e3ec21102c811161b75c16976d030b22ca4
inwlbty <doricat@outlook.com>
2019-04-14 22:34:17 +0800

    copy-list-with-random-pointer

--- src/cs/Problems/CopyListWithRandomPointerProblem.cs
+++ src/cs/Problems/CopyListWithRandomPointerProblem.cs
@@ -9,18 +9,14 @@ namespace Problems
         {
             var map = new Dictionary<Node, Node>();
             var node = head;
-            Node result = null;
             var current = new Node();
+            var result = head == null ? null : current;
             while (node != null && current != null)
             {
                 current.val = node.val;
                 current.next = node.next == null ? null : new Node();
                 current.random = node.random;
                 map.Add(node, current);
-                if (result == null)
-                {
-                    result = current;
-                }
                 node = node.next;
                 current = current.next;
             }
