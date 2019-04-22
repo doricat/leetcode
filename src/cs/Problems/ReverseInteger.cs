d09bcb05937cbfd4cae8486d2300b869c87889dc
inwlbty <doricat@outlook.com>
2019-04-22 20:47:20 +0800

    reverse-integer

---
+++ src/cs/Problems/ReverseInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problems
+{
+    public class ReverseInteger
+    {
+        // https://leetcode.com/problems/reverse-integer/
+        public static int Reverse(int x)
+        {
+            var result = 0;
+            while (x != 0)
+            {
+                if (Math.Abs(result) > int.MaxValue / 10)
+                {
+                    return 0;
+                }
+
+                result = result * 10 + x % 10;
+                x /= 10;
+            }
+
+            return result;
+        }
+    }
+}
